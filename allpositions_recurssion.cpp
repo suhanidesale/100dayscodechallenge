59d861807fec804a3251709b9f27034e57e13a5c
Suhani Desale <deslesuhani02@gmail.com>
2021-12-29 12:59:31 +0530

    Add files via upload

--- allpositions_recurssion.cpp
+++ allpositions_recurssion.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void printallindexes(int a[] , int n , int x, int i)
+void printallindexes(int a[], int n, int x, int i)
 {
     if (i == n)
     {
@@ -9,17 +10,60 @@ void printallindexes(int a[] , int n , int x, int i)
     }
     if (a[i] == x)
     {
-        cout<<i<<" ";
+        cout << i << " ";
     }
-    printallindexes(a,n,x,i+1);
-
-
+    printallindexes(a, n, x, i + 1);
 }
 
+void printallindexes1(int a[], int n, int x, int i, vector<int> &ans)
+{
+    if (i == n)
+    {
+        return;
+    }
+    if (a[i] == x)
+    {
+        ans.push_back(i);
+    }
+    printallindexes1(a, n, x, i+1,ans);
+}
 
-int main(){
-    int a[] = {1,4,5,5,7};
-    printallindexes(a,5,5,1);
+int storeallindexes(int a[], int n, int x, int i, int output[], int fsf)
+{
+    if (i == n)
+    {
+        return 0;
+    }
+    if (a[i] == x)
+    {
+        output[fsf] = i;
+        return 1 + storeallindexes(a, n, x, i + 1, output, fsf+1);
+    }
+    else
+    {
+        return storeallindexes(a, n, x, i + 1, output, fsf);
+    }
+    
+}
 
+int main()
+{
+    int a[] = {1, 4, 5, 5, 7};
+   vector<int> v;
+
+    printallindexes1(a, 5, 5, 0, v);
+
+    for (int i = 0; i <v.size() ; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout<<endl;
+    int output[10];
+    int f_count = storeallindexes(a,5,5,0,output,0);
+    for (int j = 0; j < f_count ; j++)
+    {
+        cout<<output[j]<<" ";
+    }
+    
     return 0;
 }
