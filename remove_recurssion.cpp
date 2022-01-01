90456ca6b59817d49b95deee20ec347eda7264f3
Suhani Desale <deslesuhani02@gmail.com>
2022-01-01 15:19:03 +0530

    Add files via upload

---
+++ remove_recurssion.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void remove(char a[])
+{
+    if (a[0] == '\0')
+    {
+        return;
+    }
+
+    if(a[0] != 'a')
+    {
+        remove(a+1);
+    }
+    else{
+        for (int i = 0; a[i] != '\0'; i++)
+        {
+            a[i] = a[i+1];
+        }
+        remove(a);
+    }
+}
+
+
+int main()
+{
+
+    char a[] = "suhani";
+    cout<<a<<endl;
+    remove(a);
+    cout<<a<<endl;
+
+    return 0;
+
+}
